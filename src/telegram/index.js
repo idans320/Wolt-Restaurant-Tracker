8fa598818702ecc941ebb589a624df520b153bf4
Idan Shani <idan.shani32@gmail.com>
2021-08-03 21:55:18 +0300

    avoiding telegram as global constant

--- src/telegram/index.js
+++ src/telegram/index.js
@@ -4,9 +4,7 @@ const token = process.env.TELEGRAM_TOKEN;
 
 const chat_id = process.env.TELEGRAM_CHAT_ID;
 
-const bot = new TelegramBot(token);
-
-
 export const sendMessage = (text) => {
+    const bot = new TelegramBot(token);
     bot.sendMessage(chat_id,text)
 }
