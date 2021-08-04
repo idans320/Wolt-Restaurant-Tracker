fb5787079c9adf27e0aa2aad89d305f16ab33f47
Idan Shani <idan.shani32@gmail.com>
2021-08-04 23:55:05 +0300

    garbage collector

--- src/index.js
+++ src/index.js
@@ -4,19 +4,22 @@ import * as EventHandler from "./EventHandler"
 async function main() {
     //This promise will resolve when the network call succeeds
     //Feel free to make a REST fetch using promises and assign it to networkPromise
-    const TrackedRestaurant = new Restaurant(process.env.WOLT_RESTAURANT)
-    const isAvailable = await TrackedRestaurant.getIsAvailable()
-    if (isAvailable)
-        EventHandler.emitRestaurantAvailable(TrackedRestaurant)
-    else
-        EventHandler.emitRestaurantUnavailable(TrackedRestaurant)
-
+    const trackStatus = async () => {
+        const TrackedRestaurant = new Restaurant(process.env.WOLT_RESTAURANT)
+        const isAvailable = await TrackedRestaurant.getIsAvailable()
+        if (isAvailable)
+            EventHandler.emitRestaurantAvailable(TrackedRestaurant)
+        else
+            EventHandler.emitRestaurantUnavailable(TrackedRestaurant)
+    }
 
     //This promise will resolve when 2 seconds have passed
     const timeOutPromise = new Promise(function (resolve, reject) {
         // 2 Second delay
-        setTimeout(resolve, 2000, 'Timeout Done');
+        setTimeout(resolve, 60000, 'Timeout Done');
     });
+
+    await trackStatus()
 
     await timeOutPromise;
 
