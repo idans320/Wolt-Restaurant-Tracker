5ead6bf2ab8faa398c65554e90551e89ad05191f
Idan Shani <idan.shani32@gmail.com>
2021-08-07 21:25:31 +0300

    fix tests

--- tests/restaurant.test.js
+++ tests/restaurant.test.js
@@ -1,39 +1,34 @@
-import {Restaurant} from "../src/services/wolt/restaurants";
+import { Restaurant } from "../src/services/wolt/restaurants";
 import fs from 'fs'
-import {JSDOM} from "jsdom"
+import { JSDOM } from "jsdom"
 import path from "path"
 
 const GDBRestaurant = new Restaurant("gdb");
 
+
 GDBRestaurant.getHtml = async () => {
-	
-	const html = await fs.promises.readFile(path.join(__dirname,"res/GDB.html"))
-	const dom = new JSDOM(html)
-	return dom
-} 
+
+	const html = await fs.promises.readFile(path.join(__dirname, "res/GDB.html"))
+	return html
+}
 
 const lilush = new Restaurant("lilush");
 
 lilush.getHtml = async () => {
-	
-	const html = await fs.promises.readFile(path.join(__dirname,"res/lilush.html"))
+
+	const html = await fs.promises.readFile(path.join(__dirname, "res/lilush.html"))
 	return html
-} 
+}
 
 
-GDBRestaurant.getHtml = async () => {
-	
-	const html = await fs.promises.readFile(path.join(__dirname,"res/GDB.html"))
-	return html
-} 
 
 
 test('correct status unavailable', async () => {
-	  const status = await GDBRestaurant.getIsAvailable()
+	const status = await GDBRestaurant.getIsAvailable()
 	expect(status).toBe(false)
 });
 
 test('correct status available', async () => {
 	const status = await lilush.getIsAvailable()
-  expect(status).toBe(true)
+	expect(status).toBe(true)
 });
